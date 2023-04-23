e6cfba49b8fb95ce455e85feb75e995a164686b2
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-04-23 17:40:26 +0530

    Hackerrank solution

--- Accenture/q2.java
+++ Accenture/q2.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class q2 {
     public static void main(String[] args) {
         int [] arr1 = {1,2,3,4};
-        int [] arr2 = {1,2,3,4,5};
+        int [] arr2 = {1,2,3,4,6};
         int a1 = solve(arr1);
         int a2 = solve(arr2);
         System.out.println(a1 + " " + a2);
