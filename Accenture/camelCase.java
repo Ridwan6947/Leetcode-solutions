8973374f57821c19370791577ca0eee5ce54f97a
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-05-22 17:46:59 +0530

    Practice QUestion

--- Accenture/camelCase.java
+++ Accenture/camelCase.java
@@ -4,7 +4,7 @@ import java.lang.*;
 public class camelCase {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = "saveChangesInTheEditor";
+        String str = "saveChangesInTheEditors";
         String ans = "";
         for(int i = 0 ; i < str.length() ; i++){
             char c = str.charAt(i);
