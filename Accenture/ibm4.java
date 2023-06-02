0821476bfb25bd33005c05f54cf9b03075f68268
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-06-02 07:33:25 +0530

    practice

--- Accenture/ibm4.java
+++ Accenture/ibm4.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class ibm4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 2;
+        int n = 3;
         solve(n);
     }
     public static void solve(int n){
