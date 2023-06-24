79aceaeb9ad04d164f3496bdb6a4f7bbeeb9084a
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-06-24 18:58:10 +0530

    RunTime polymorphism

---
+++ Accenture/runTime.java
@@ -0,0 +1,16 @@
+class Bike{  
+  public void run(){
+        System.out.println("running");
+    }  
+}  
+class Splendor extends Bike{
+    public void run(){
+        System.out.println("running safely with 60km");
+    }
+}  
+public class runTime{
+    public static void main(String args[]){  
+        Bike b = new Splendor();//upcasting  
+        b.run();  
+    }  
+}
