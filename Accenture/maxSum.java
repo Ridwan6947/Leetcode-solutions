4b2bcfad241316a7297e1aa55ae5394fadb36afc
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-08-27 19:30:28 +0530

    new paper sol

---
+++ Accenture/maxSum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class maxSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int []arr = {2,-3,4,6,1};
+        // for(int i = 0 ; i < arr.length ; i++){
+        //     arr[i] = sc.nextInt();
+        // }
+        int k = 2;
+        int ans = solve(arr,k);
+        System.out.println(ans);
+    }
+    public static int solve(int[] arr , int k){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i < arr.length ; i++){
+            int j = i;
+            int currMax = 0;
+            while(j < arr.length){
+                // System.out.println(arr[i]);
+                currMax += arr[j];
+                j +=k;
+            }
+            // System.out.print(max + " " );
+            max = Math.max(max,currMax);
+        }
+        return max;
+    }
+}
