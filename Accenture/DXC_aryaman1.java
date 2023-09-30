79840ee936f5df3ce746e8e0692505607840fbeb
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-09-30 18:30:45 +0530

    DXC questions

---
+++ Accenture/DXC_aryaman1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DXC_aryaman1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 6;
+        char arr[] = new char[a];
+        arr= {'a' , 'v' , 'i' , 'k' , 'e' , 'l'};
+
+
+    }
+
+}
