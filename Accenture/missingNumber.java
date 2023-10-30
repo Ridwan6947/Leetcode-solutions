9b5560b8395cf763e167242609a3296b4936ec3b
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-10-30 17:05:55 +0530

    gfg

--- Accenture/missingNumber.java
+++ Accenture/missingNumber.java
@@ -1,5 +1,5 @@
 class Solution {
-    int missingNumber(int array[], int n) {
+    public static int missingNumber(int array[], int n) {
         int tot=(n*(n+1))/2;
         int sum=0;
         for(int i=0;i<n-1;i++){
@@ -7,5 +7,11 @@ class Solution {
         }
         int res=tot-sum;
         return res;
+    }
+    public static void main(String[] args) {
+        int []arr = {1,2,3,5};
+        int ans = missingNumber(arr, arr.length);
+        System.out.println(ans);
+
     }
 }
