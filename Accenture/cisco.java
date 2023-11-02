6e3a0ec0354195ef7cb5cce2e74fc9d5471bf5d3
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-11-02 20:12:30 +0530

    CISCO Question

---
+++ Accenture/cisco.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class cisco {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        String pattern = scanner.nextLine();
+        scanner.close();
+        
+        int uniqueNumber = findUniqueNumber(inputString, pattern);
+        System.out.println(uniqueNumber);
+    }
+
+    public static int findUniqueNumber(String input_String, String pattern) {
+        int uniqueNumber = 0;
+        int patternLength = pattern.length();
+
+        for (int i = 0; i <= input_String.length() - patternLength; i++) {
+            boolean match = true;
+            for (int j = 0; j < patternLength; j++) {
+                if (pattern.charAt(j) != '#' && pattern.charAt(j) != input_String.charAt(i + j)) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match) {
+                uniqueNumber = uniqueNumber * 10 + (i + 1);
+            }
+        }
+
+        return uniqueNumber;
+    }
+}
