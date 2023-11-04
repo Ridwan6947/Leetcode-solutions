67d52f2bc3c737c1163d23d03258e5ecdd8c71bc
Syed Ridwan Ahmad <81156501+Ridwan6947@users.noreply.github.com>
2023-11-04 23:12:55 +0530

    GFG Equilibrium point

---
+++ Accenture/equilibriamPoint.java
@@ -0,0 +1,23 @@
+public class equilibriamPoint{
+    public static void main(String[] args) {
+        int arr[] = {1,3,5,2,2};
+        int n = arr.length;
+        System.out.print(solve(arr,n));
+    }
+    public static int solve(int arr[] , int n){
+        int  totalSum=0;
+        for(long num:arr){
+             totalSum+=num;
+        }
+         int leftSum=0;
+         for(int i=0;i<n;i++){
+             totalSum-=arr[i];
+              if(leftSum==totalSum){
+                return i+1;
+              }
+               leftSum += arr[i];
+         }
+        return -1;
+        // System.out.println(leftSum + " " + rightSum + " " + arr[left+1]);
+    }
+}
